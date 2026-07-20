4d3aeddd88d25a78ec345cb5d73cf964e6dd4c1f
moktiksalgotra <moktik1271@gmail.com>
2026-07-20 22:07:05 +0530

    Add config-server service

--- config-server/src/main/java/com/tnf/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/tnf/config_server/ConfigServerApplication.java
@@ -2,13 +2,11 @@ package com.tnf.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-// PORT : 8888
-
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 @EnableDiscoveryClient
 public class ConfigServerApplication {
 
